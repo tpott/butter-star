8eb8e939c395b6b276b80d28688bc69ac57fc9e3
Rohan Halliyal <rohan.halliyal@gmail.com>
2013-04-07 16:09:17 -0700

    Update websocket.js

--- client/websocket.js
+++ client/websocket.js
@@ -1,10 +1,17 @@
+// Create the connection
 var connection = new WebSocket('ws://128.54.45.206:8080');
 var testObj = {data1 : 'moo', data2: 10};
 
+/* Once the connection is etablished, this method will be called. Sends our testObj
+ * over the socket
+*/
 connection.onopen = function () {
   connection.send(JSON.stringify(testObj)); // Test msg sent to server
 };
 
+/* On receive of a message, take the msg's data and parse that (we assume 
+ * messages that are transmitted will be encoded in JSON format.
+ */
 connection.onmessage = function(msg) {
   var received = JSON.parse(msg.data);
   console.log('Yo server said: ' + received);
@@ -14,5 +21,7 @@ connection.onmessage = function(msg) {
 connection.onerror = function(error) {
   console.log('WebSocket Error: ' + error);
 };
+
+
 
 
