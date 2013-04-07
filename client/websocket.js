5e506dd752bf0dfa63a5e8b24db6bb953ea8ed0d
Jennifer Fang <jyfang@cse125.ucsd.edu>
2013-04-07 15:26:34 -0700

    Can send objects to and from server.

--- client/websocket.js
+++ client/websocket.js
@@ -1,9 +1,18 @@
-
-var connection = new WebSocket('ws://128.54.45.206:8080', 
-	['soap', 'xmpp']);
+var connection = new WebSocket('ws://128.54.45.206:8080');
+var testObj = {data1 : 'moo', data2: 10};
 
 connection.onopen = function () {
-  connection.send('Sup sup sup sup'); // Send the message 'Ping' to the server
+  connection.send(JSON.stringify(testObj)); // Test msg sent to server
+};
+
+connection.onmessage = function(msg) {
+  var received = JSON.parse(msg.data);
+  console.log('Yo server said: ' + received);
+};
+
+// Log errors
+connection.onerror = function(error) {
+  console.log('WebSocket Error: ' + error);
 };
 
 
