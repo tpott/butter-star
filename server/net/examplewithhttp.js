2cdabf65dbef53a8dc5c1f85556d72b914fe4548
Trevor Pottinger <tpott3@gmail.com>
2013-04-09 00:31:50 -0700

    Added a Vector4 object and a function for sending it to the client.

--- server/net/examplewithhttp.js
+++ server/net/examplewithhttp.js
@@ -52,20 +52,45 @@ fs.readFile(jsFile, 'utf8', function(err, data) {
 	if (readCount == 2) createServer();
 });
 
+// OBJECT ORIENTED
+function Vector4() {
+	this.arr = new Float32Array(4);
+	this.byteLength = 16;
+}
+
+// this copies the buffer into the vector4
+Vector4.prototype.set = function (buffer) {
+	var intarr = new Uint8Array(this.arr.buffer);
+	for (var i = 0; i < this.byteLength; i++) {
+		intarr[i] = buffer[i];
+	}
+};
+
 // WEB-SOCKETS
 
 var wsServer = new ws({ port:wsPort});
 sockets = [];
+vectors = [];
 buffers = [];
+
+/**
+ * sends a Vector4 object over specified socket
+ */
+vecsend = function(socket, vec) {
+	// access the Vector4's array's buffer
+	socket.send(vec.arr.buffer, {binary:true});
+}
 
 // Create the server, once connection is established then execute the function
 wsServer.on('connection', function(socket) {
 	console.log('New connection created! %d', sockets.length);
 	sockets[sockets.length] = socket; 
+	vectors[vectors.length] = new Vector4();
 	
 	socket.on('message', function(buf) {
 		console.log(buf);
 		buffers[buffers.length] = buf;
+		vectors[vectors.length-1].set(buf);
 	});
 });
 
