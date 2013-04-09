09129d7af5e35b79bb5baee1917f3cf6fe57d7ab
Jennifer Fang <jyfang@cse125.ucsd.edu>
2013-04-08 21:34:23 -0700

    Test sending float array

--- client/net/websocket.js
+++ client/net/websocket.js
@@ -1,5 +1,7 @@
 /**
  * Sets up connection and communication to and from the server.
+ * @author Jennifer Fang
+ * @author Trevor Pottinger
  */
 
 /**
@@ -21,20 +23,23 @@ if(localStorage.getItem('butterIPAddr') == null) {
  * @type {WebSocket}
  */
 var connection = new WebSocket('ws://' + ipAddr + ':8080');
+connection.binaryType = 'arrayBuffer';
+
+var floatArr = new Float64Array(4);
 
 /**
  * Handles opening a connection to the server.
  */
 connection.onopen = function () {
-  connection.send('wassupppp'); // Test msg sent to server
+  connection.send(floatArr.buffer); // Test msg sent to server
 };
 
 /**
  * Handles receiving messages from the server.
  * @param {ArrayBuffer} msg The array from the server.
  */
-connection.onmessage = function(msg) {
-  console.log('Yo server said: ' + msg.data);
+connection.onmessage = function(buf) {
+  console.log('Yo server said: ' + buf);
 };
 
 /**
