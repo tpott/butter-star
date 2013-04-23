5540189b251fc0c7da0ce9960723b6a80baf548b
Thinh Nguyen <thn024@gmail.com>
2013-04-22 20:37:47 -0700

    gravity

--- server/physics/gravity.js
+++ server/physics/gravity.js
@@ -13,11 +13,14 @@ var THREE = require('three');
  */
 function Gravity() {
   // TODO get passed from client? diff maps
-  var geometry = new THREE.PlaneGeometry(200, 200, 100, 100);
+  var geometry = new THREE.PlaneGeometry(200,200,1,1);
   geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
   geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, -10, 0));
   var material = new THREE.MeshBasicMaterial();
   this.floor = new THREE.Mesh(geometry, material);
+  //this.floor.matrixWorld.makeRotationX( -Math.PI / 2);
+  //this.floor.matrixWorld.makeTranslation(0,-10,0);
+  
 };
 
 /**
@@ -27,24 +30,24 @@ function Gravity() {
 Gravity.prototype.applyGravity = function(obj) {
   var raycaster = new THREE.Raycaster();
   raycaster.ray.direction.set(0, -1, 0);
-  var intersections = raycaster.intersectObject(ground);
-  raycaster.ray.origin.copy(obj.position.x, obj.position.y, obj.position.z);
+  raycaster.ray.origin.set(obj.position.x, obj.position.y, obj.position.z);
+ console.log(JSON.stringify(obj.position)); 
   // raycaster.ray.origin.y -= 9.8; // TODO move to individual obj for diff grav
-
   var isOnGround = false;
-  var intersections = raycaster.intersectObject(this.floor);
-  console.log(JSON.stringify(intersections));
+  var intersections = raycaster.intersectObjects([this.floor, this.floor]);
+  //console.log(JSON.stringify(intersections));
+  //console.log([this.floor]);
   if (intersections.length > 0) {
     console.log('GRAVITYYYY');
     var distance = intersections[0].distance;
 
-    if(distance > 0 && distance < 10) {
+    if(distance > 0 && distance <= 1) {
       isOnGround = true;
     }
   }
 
   if(isOnGround === true) {
-    obj.translate(0, -10, 0); // TODO fix later for objs of diff heights
+    //obj.translate(0, -10, 0); // TODO fix later for objs of diff heights
   } else {
     console.log('falling but not really!');
     obj.translate(0, -1, 0);
