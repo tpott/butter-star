a9bf5497c400c2db292033d438ca643208362e9a
Trevor Pottinger <tpott3@gmail.com>
2013-05-08 19:01:49 -0700

    Still working

--- server/objects/critter.js
+++ server/objects/critter.js
@@ -13,24 +13,6 @@ var Critter = function(){
 Critter.prototype.init = function() {
 }
 
-Critter.prototype.initModel = function(scene, type, size, position) {
-    var loader = new THREE.OBJMTLLoader();
-                loader.addEventListener( 'load', function ( event ) {
-                    var object = event.content;
-                    var tempScale = new THREE.Matrix4();
-                    object.position.x = position.y;
-                    object.position.y = position.x;
-                    object.position.z = position.z;
-                    
-
-                    object.scale.set(size * .08 ,size * .08,size * .08);
-    });
-    if(type == "boo")
-    {
-        loader.load( 'boo.obj', 'boo.mtl' );
-    }
-}
-
 Critter.prototype.update = function() {
 
 };
